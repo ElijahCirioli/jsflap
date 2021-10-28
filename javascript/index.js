12b843cb17296292e138b67500c9bdaf9b125670
Elijah Cirioli <50853223+ElijahCirioli@users.noreply.github.com>
2021-10-28 14:04:44 -0700

    added new tab button

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,31 @@
 let environments = [];
+let activeEnvironment = undefined;
 
 function createEnvironment() {
 	unselectAllEnvironments();
 	const newTab = $("<div class='active environment-tab'>Untitled 1</div>");
-	$("#environment-tab-bar").append(newTab);
+	$("#new-environment-button").before(newTab);
 	const newEnv = new Environment(newTab);
 	environments.push(newEnv);
+	activeEnvironment = newEnv;
 	newTab.click((e) => {
 		unselectAllEnvironments();
 		newEnv.getContent().show();
 		newTab.addClass("active");
+		activeEnvironment = newEnv;
 	});
 }
 
 function unselectAllEnvironments() {
 	$(".environment-tab").removeClass("active");
 	$(".environment-wrap").hide();
+	activeEnvironment = undefined;
 }
 
 $("document").ready(() => {
 	createEnvironment();
+
+	$("#new-environment-button").click((e) => {
+		createEnvironment();
+	});
 });
