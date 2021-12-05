c8630c4616288aa7b39b12efcd62dc6f3674a3e8
Elijah Cirioli <50853223+ElijahCirioli@users.noreply.github.com>
2021-12-04 19:15:18 -0800

    Improved parsing algorithm

--- javascript/automata/pushdown/pushdownAutomaton.js
+++ javascript/automata/pushdown/pushdownAutomaton.js
@@ -37,42 +37,47 @@ class PushdownAutomaton extends Automaton {
 
 	languageContains(word) {
 		// is the word in the language of this FSA?
+
+		// make sure there's an initial state
 		if (!this.initialState) {
 			return false;
 		}
 
+		// step through instanenous descriptions in a breadth-first manner
 		const queue = [{ word: word, stack: "Z", state: this.initialState }];
 		const visited = new Set();
 		visited.add(this.getInstantaneousDescriptionKey(queue[0]));
+		let numConfigurations = 0;
+
 		while (queue.length > 0) {
-			if (queue.length > 500) {
-				console.log("overflow");
-				return;
+			numConfigurations++;
+			if (numConfigurations > 500) {
+				return undefined;
 			}
 
+			// take from front of the queue
 			const curr = queue.shift();
-			console.log(curr);
-
+			// see if we're at an accept state
 			if (curr.word.length === 0 && curr.state.isFinal()) {
+				console.log(numConfigurations);
 				return true;
 			} else {
+				// look at every other state we can go to
 				curr.state.getTransitions().forEach((t) => {
 					const toState = t.getToState();
+					// look at every tuple we can use to get there
 					t.getLabels().forEach((tuple) => {
-						const next = { word: undefined, stack: undefined, state: toState };
-						const char = curr.word.length > 0 ? curr.word.substring(0, 1) : "";
-						if (tuple.char === "" || tuple.char === char) {
-							next.word = tuple.char === "" ? curr.word : curr.word.substring(1);
-							const stackPop = curr.stack.length > 0 ? curr.stack.substring(curr.stack.length - 1) : "";
-							if (tuple.pop === "" || tuple.pop === stackPop) {
-								next.stack = tuple.pop === "" ? curr.stack + tuple.push : curr.stack.substring(0, curr.stack.length - 1) + tuple.push;
-								const key = this.getInstantaneousDescriptionKey(next);
-								if (!visited.has(key)) {
-									visited.add(key);
-									queue.push(next);
-								} else {
-									console.log("found duplicate: " + key);
-								}
+						if (this.canUseTransitionTuple(curr, tuple)) {
+							const newWord = tuple.char === "" ? curr.word : curr.word.substring(1);
+							const newStack = tuple.pop === "" ? tuple.push + curr.stack : tuple.push + curr.stack.substring(1);
+
+							// make sure we haven't computed this description before
+							const next = { word: newWord, stack: newStack, state: toState };
+							const key = this.getInstantaneousDescriptionKey(next);
+							if (!visited.has(key)) {
+								// add to queue
+								visited.add(key);
+								queue.push(next);
 							}
 						}
 					});
@@ -80,6 +85,13 @@ class PushdownAutomaton extends Automaton {
 			}
 		}
 		return false;
+	}
+
+	canUseTransitionTuple(desc, tuple) {
+		const charMatches = tuple.char === "" || (desc.word.length > 0 && tuple.char === desc.word.substring(0, 1));
+		const popMatches = tuple.pop === "" || (desc.stack.length > 0 && tuple.pop === desc.stack.substring(0, 1));
+
+		return charMatches && popMatches;
 	}
 
 	getInstantaneousDescriptionKey(tuple) {
