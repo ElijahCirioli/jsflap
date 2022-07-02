d490de6e3b3699de11368e6aaf06e2dc5eb01bd9
Elijah Cirioli <50853223+ElijahCirioli@users.noreply.github.com>
2022-07-01 23:10:07 -0700

    creating step-by-step table

--- javascript/gui/stepInputContainer.js
+++ javascript/gui/stepInputContainer.js
@@ -2,6 +2,7 @@ class StepInputContainer {
 	constructor(content, callback) {
 		this.stepWrap = content.children(".environment-sidebar").children(".step-wrap");
 		this.layers = [];
+		this.margin = 20;
 
 		this.setupContainer();
 		this.setupListeners();
@@ -12,7 +13,7 @@ class StepInputContainer {
         <h1 class="environment-sidebar-title">Step-by-step test</h1>
         <div class="step-input">
             <form class="inputs-form">
-                <input type="text" spellcheck="false" maxlength="256" class="inputs-form-item-input single-line-input">
+                <input type="text" spellcheck="false" maxlength="256" placeholder="Input word" class="inputs-form-item-input single-line-input">
             </form>
             <div class="inputs-buttons-wrap">
                 <button class="inputs-button inputs-lambda-button">${lambdaChar}</button>
@@ -23,47 +24,71 @@ class StepInputContainer {
             <div class="step-tree-nodes-wrap"></div>
             <canvas class="step-tree-canvas" width="225" height="100"></canvas>
         </div>
-        <div class="step-table">
+        <div class="step-tree-buttons-wrap"></div>
+        <div class="step-table-wrap">
+            <table class="step-table"></table>
         </div>`;
 
 		this.canvas = this.stepWrap.children(".step-tree-wrap").children("canvas");
 		this.context = this.canvas[0].getContext("2d");
 		this.nodesWrap = this.stepWrap.children(".step-tree-wrap").children(".step-tree-nodes-wrap");
+		this.table = this.stepWrap.children(".step-table-wrap").children("table");
 	}
 
-	setupListeners() {}
+	setupListeners() {
+		const textInput = this.stepWrap.children(".step-input").children(".inputs-form").children("input");
+		textInput.on("keyup change", (e) => {
+			const word = textInput.val();
+
+			if (word.length === 0) {
+				this.restoreDefault();
+				return;
+			}
+
+			const editor = activeEnvironment.getEditor();
+			if (!editor) {
+				return;
+			}
+
+			const parseSteps = editor.getAutomaton().getParseSteps(word);
+			if (parseSteps.length > 0) {
+				this.drawTree(parseSteps);
+			}
+		});
+	}
 
 	drawTree(layers) {
 		this.layers = layers;
+		let nodeHeight = 20;
 
-		// resize canvas
-		const margin = 20;
-		const defaultWidth = this.stepWrap.children(".step-tree-wrap").width() - 2 * margin;
-		const defaultHeight = this.stepWrap.children(".step-tree-wrap").height() - 2 * margin;
-
-		let nodeWidth = 24;
+		// calculate required width to fit all nodes
+		const defaultWidth = this.stepWrap.children(".step-tree-wrap").width() - 2 * this.margin;
+		const defaultHeight =
+			parseInt(this.stepWrap.children(".step-tree-wrap").css("min-height")) - 2 * this.margin;
 		for (let i = 1; i < layers.length; i++) {
 			const numChildren = new Array(layers[i - 1].length).fill(0);
 			for (const step of layers[i]) {
 				numChildren[step.predecessor]++;
 			}
-			nodeWidth *= Math.max(...numChildren);
+			nodeHeight *= Math.max(...numChildren);
 		}
 
-		const width = Math.max(nodeWidth, defaultWidth);
-		const height = Math.max(layers.length * 28 + 2 * margin, defaultHeight);
-		this.canvas.attr("width", width + 2 * margin);
-		this.canvas.attr("height", height);
+		// resize canvas
+		const width = Math.max(layers.length * 26, defaultWidth);
+		const height = Math.max(nodeHeight, defaultHeight);
+		this.canvas.attr("width", width + 2 * this.margin);
+		this.canvas.attr("height", height + 2 * this.margin);
 
 		// create the nodes
 		this.nodesWrap.empty();
 
 		// setup head node
 		const head = layers[0][0];
-		head.width = width;
-		head.leftEdge = 0;
-		this.createNode(head, new Point(width / 2, 0));
+		head.height = height;
+		head.topEdge = 0;
+		this.createNode(head, new Point(0, height / 2));
 
+		// position one layer at a time
 		for (let i = 1; i < layers.length; i++) {
 			// create the groupings based on parent nodes
 			const groups = new Map();
@@ -81,32 +106,104 @@ class StepInputContainer {
 				const parent = layers[i - 1][parentIndex];
 				for (let j = 0; j < steps.length; j++) {
 					const step = steps[j];
-					step.width = parent.width / steps.length;
-					step.leftEdge = (j / steps.length) * parent.width + parent.leftEdge;
-					const xPos = step.leftEdge + step.width / 2;
-					console.log(i, j, step.width, step.leftEdge, xPos);
-					this.createNode(step, new Point(xPos, i * 30));
+					step.height = parent.height / steps.length;
+					step.topEdge = (j / steps.length) * parent.height + parent.topEdge;
+					const yPos = step.topEdge + step.height / 2;
+					this.createNode(step, new Point(i * 30, yPos));
 				}
 			});
+		}
 
-			// draw the lines
-			for (const step of layers[i]) {
+		this.drawLines();
+		this.centerView(head);
+	}
+
+	createNode(step, pos) {
+		const node = $(`<div class="step-tree-node"></div>`);
+		step.canvasPos = new Point(pos.x + this.margin, pos.y + this.margin);
+		node.css("left", step.canvasPos.x - 7 + "px");
+		node.css("top", step.canvasPos.y - 7 + "px");
+
+		if (step.accept === true) {
+			node.addClass("step-tree-node-accept");
+		} else if (step.accept === false) {
+			node.addClass("step-tree-node-reject");
+		}
+
+		// add to div
+		this.nodesWrap.append(node);
+
+		// create listeners
+		node.on("mouseenter", (e) => {
+			this.highlightLayer(step.canvasPos.x);
+			this.populateTable(this.layers[step.depth]);
+		});
+
+		node.on("mouseleave", (e) => {
+			this.drawLines();
+		});
+	}
+
+	drawLines() {
+		this.context.clearRect(0, 0, this.canvas.width(), this.canvas.height());
+		this.context.lineWidth = 2;
+		this.context.strokeStyle = "#2c304d";
+		for (let i = 1; i < this.layers.length; i++) {
+			for (const step of this.layers[i]) {
 				this.context.beginPath();
 				this.context.moveTo(step.canvasPos.x, step.canvasPos.y);
-				const parent = layers[i - 1][step.predecessor];
+				const parent = this.layers[i - 1][step.predecessor];
 				this.context.lineTo(parent.canvasPos.x, parent.canvasPos.y);
 				this.context.stroke();
 			}
 		}
 	}
 
-	createNode(step, pos) {
-		const margin = 20;
-		const node = $(`<div class="step-tree-node"></div>`);
-		step.canvasPos = new Point(pos.x + margin, pos.y + margin);
-		node.css("left", step.canvasPos.x - 7 + "px");
-		node.css("top", step.canvasPos.y - 7 + "px");
+	highlightLayer(xPos) {
+		this.drawLines();
+		this.context.fillStyle = "rgba(250, 214, 60, 0.5)";
+		this.context.fillRect(xPos - 10, 0, 20, this.canvas.height());
+	}
 
-		this.nodesWrap.append(node);
+	populateTable(layer) {
+		this.table.empty();
+
+		const editor = activeEnvironment.getEditor();
+		if (!editor) {
+			return;
+		}
+
+		// create the header
+		const header = $("<tr><th>State</th><th>Input</th></tr>");
+		if (editor instanceof PushdownEditor) {
+			header.append("<th>Stack</th>");
+		}
+		this.table.append(header);
+
+		// add the steps
+		for (const step of layer) {
+			const row = $(`<tr><td>${step.state.getName()}</td><td>${step.word}</td></tr>`);
+			if (editor instanceof PushdownEditor) {
+				row.append(`<td>${step.stack}</td>`);
+			}
+			this.table.append(row);
+		}
+	}
+
+	centerView(step) {
+		const height = this.stepWrap.children(".step-tree-wrap").height();
+		const scroll = {
+			scrollTop: step.canvasPos.y - height / 2,
+			scrollLeft: step.canvasPos.x - this.margin,
+		};
+
+		this.stepWrap.children(".step-tree-wrap").animate(scroll, 0);
+	}
+
+	restoreDefault() {
+		this.canvas.attr("width", 225);
+		this.canvas.attr("height", 100);
+		this.nodesWrap.empty();
+		this.context.clearRect(0, 0, this.canvas.width(), this.canvas.height());
 	}
 }
